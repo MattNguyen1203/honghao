89e600635705ca6a8aaea7c05c417a55d90d0ace
Matt <huongax123@gmail.com>
2024-05-17 09:15:16 +0700

    update itemtour

---
+++ app/global/page.jsx
@@ -0,0 +1,14 @@
+import ItemTour from '@/components/itemtour'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mt-[20rem]'>
+      <div className='w-[21.375rem] ml-[2rem]'>
+        <ItemTour />
+      </div>
+    </div>
+  )
+}
+
+export default page
