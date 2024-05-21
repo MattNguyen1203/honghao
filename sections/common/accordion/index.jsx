834a2f4dc26dd32b308fc86e5b4ec16d746217da
kKao2 <thanh291100@gmail.com>
2024-05-21 16:52:15 +0700

    commit

--- sections/common/accordion/index.jsx
+++ sections/common/accordion/index.jsx
@@ -24,7 +24,6 @@ const AccordionCustom = ({data}) => {
             <AccordionTrigger className='text-1 xmd:text-0875 font-bold tracking-[0.0125rem] text-greyscale-30 py-[0.5rem] hover:no-underline border-b border-solid border-[#828282] [&[data-state=open]]:border-orange-normal [&[data-state=open]]:text-orange-normal'>
               Is it accessible?
             </AccordionTrigger>
-            <AccordionContent className='text-1 tracking-[0.0125rem] text-greyscale-40 py-2 md:py-4'>
             <AccordionContent className='!text-1 xmd:!text-0875 tracking-[0.0125rem] text-greyscale-40 pt-[1rem]'>
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
