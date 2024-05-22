c0417aa596f6f2d389a062f652524a9ccaff9c7f
Matt <huongax123@gmail.com>
2024-05-22 09:07:02 +0700

    update homepage

--- app/(store)/store.js
+++ app/(store)/store.js
@@ -1,11 +1,21 @@
 import {create} from 'zustand'
 const useStore = create((set) => ({
   isMobile: false,
+  isTablet: false,
   setIsMobile: (data) => {
     set((state) => {
       return {
         ...state,
         isMobile: data,
+      }
+    })
+  },
+
+  setIsTablet: (data) => {
+    set((state) => {
+      return {
+        ...state,
+        isTablet: data,
       }
     })
   },
--- sections/homepage/Banner.jsx
+++ sections/homepage/Banner.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Banner = () => {
-  return <div className='w-screen h-screen bg-green-normal flex'>Banner</div>
+  return (
+    <section className='w-screen h-screen bg-green-normal/20 flex relative'>
+      <div className='text-075 text-greyscale-0 font-bold tracking-[0.00938rem] absolute top-1/2 left-[1.87rem] rotate-90'>
+        Hong Hao Travel
+      </div>
+    </section>
+  )
 }
 
 export default Banner
