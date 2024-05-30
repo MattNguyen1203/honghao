8ecab5767b5642d1f258d19f38c0472d60be20d8
Matt <huongax123@gmail.com>
2024-05-30 09:25:34 +0700

    update

--- app/global/page.jsx
+++ app/global/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import BikeAnimation from '@/components/bikeAnimate'
 import ItemTour from '@/components/itemtour'
-import React from 'react'
+import React, {Suspense} from 'react'
 import Banner from '@/sections/common/banner'
 import AccordionCustom from '@/sections/common/accordion'
 import Breadcrumb from '@/components/breadcrumb'
@@ -16,31 +16,33 @@ const dataTourDetail = {
 const page = () => {
   const data = new Array(5).fill(0)
   return (
-    <div className='mt-[20rem]'>
-      <div className='w-[21.375rem] ml-[2rem]'>
-        <ItemTour />
-      </div>
+    <Suspense>
+      <div className='mt-[20rem]'>
+        <div className='w-[21.375rem] ml-[2rem]'>
+          <ItemTour />
+        </div>
 
-      <div className='w-[42.8125rem]'>
-        <AccordionCustom data={data} />
-      </div>
+        <div className='w-[42.8125rem]'>
+          <AccordionCustom data={data} />
+        </div>
 
-      <Banner
-        mainImg='/imgs/common/banner.jpg'
-        mainText='/imgs/common/text.png'
-        mainTextMb='/imgs/common/textMb.png'
-      />
-
-      <BikeAnimation />
-      <Breadcrumb divider />
-      <section className='container !p-0 bg-black space-y-[2rem]'>
-        <HomeForm dataTourDetail={dataTourDetail} />
-        <HomeForm
-          isTourDetail
-          dataTourDetail={dataTourDetail}
+        <Banner
+          mainImg='/imgs/common/banner.jpg'
+          mainText='/imgs/common/text.png'
+          mainTextMb='/imgs/common/textMb.png'
         />
-      </section>
-    </div>
+
+        <BikeAnimation />
+        <Breadcrumb divider />
+        <section className='container !p-0 bg-black space-y-[2rem]'>
+          <HomeForm dataTourDetail={dataTourDetail} />
+          <HomeForm
+            isTourDetail
+            dataTourDetail={dataTourDetail}
+          />
+        </section>
+      </div>
+    </Suspense>
   )
 }
 
