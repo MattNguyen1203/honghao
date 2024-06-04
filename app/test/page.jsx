3daa4959b93b2b7ca4b160d8ae5783bbe20e5b1a
1canhhoa <nhamvanhien31102002@gmail.com>
2024-06-04 13:06:18 +0700

    abc

---
+++ app/test/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='pb-[100rem] object-cover  bottom-0 z-[3] left-0 w-full h-[239.375rem] shrink-0 bg-[linear-gradient(180deg,rgba(18,39,24,0.00)_0%,#122718_100%)]'></div>
+  )
+}
+
+export default page
